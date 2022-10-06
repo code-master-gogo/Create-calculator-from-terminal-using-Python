50358185b25695ed1e3acf819eaa8454b1f19998
Anubhav Pandey <68274957+code-master-gogo@users.noreply.github.com>
2022-10-06 14:47:35 +0530

    Add files via upload

---
+++ calculator from terminal.py
@@ -0,0 +1,14 @@
+from operator import truediv, mul, add, sub  
+
+operators = {'+': add, '-': sub, '*': mul, '/': truediv}
+
+def calculate(s):
+    if s.isdigit():
+        return float(s)
+    for c in operators.keys():
+        left, operator, right = s.partition(c)
+        if operator in operators:
+            return operators[operator](calculate(left), calculate(right))
+
+calc = input("")
+print("Answer: " + str(calculate(calc)))
